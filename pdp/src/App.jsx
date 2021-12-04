257c69b7548a9afb1164c40ef8337e02587fd0e6
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-12-04 20:04:47 -0300

    Pdp header lazy loading

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,18 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Suspense, lazy, useState } from 'react'
+import ReactDOM from 'react-dom'
 
-import "./index.scss";
+import './index.scss'
 
-import Header from "home/Header";
-import Footer from "home/Footer";
+const Header = lazy(() => import('home/Header'))
+import Footer from 'home/Footer'
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false)
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      {showHeader &&
+        <Suspense fallback={<div>Loading</div>}>
+          <Header />
+        </Suspense>
+      }
+      <button className="text-3xl p-5"
+        onClick={() => setShowHeader(!showHeader)}>Show header</button>
       <div className="my-10 ">
         PDP page content
       </div>
-    <Footer />
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+      <Footer />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById("app"))
