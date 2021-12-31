861f5d2454b498a54ff7b5ec1f389bc2ea8f241b
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-12-31 16:55:33 -0300

    Cross-platform addToCart-PDP

--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { getProductById, currency } from 'home/products'
+import placeAddToCart from 'addtocart/placeAddToCart'
 
 const PDPContent = () => {
   const { id } = useParams()
@@ -13,6 +14,14 @@ const PDPContent = () => {
       setProduct(null)
     }
   }, [id])
+
+  const addToCart = useRef(null, [product, setProduct])
+
+  useEffect(() => {
+    if (addToCart.current) {
+      placeAddToCart(addToCart.current, product.id)
+    }
+  }, [product])
 
   if(!product) return null
 
@@ -30,6 +39,7 @@ const PDPContent = () => {
             {currency.format(product.price)}
           </div>
         </div>
+        <div ref={addToCart}></div>
         <div className="mt-10">{product.description}</div>
         <div className="mt-10">{product.longDescription}</div>
       </div>
