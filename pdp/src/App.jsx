0a5d438609e7bc895fba584ff36005737182bafe
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-12-31 19:12:39 -0300

    Downgrade react-router-dom

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -6,16 +6,16 @@ import './index.scss'
 import Header from 'home/Header'
 import Footer from 'home/Footer'
 import PDPContent from './PDPContent'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => (
   <Router>
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
       <Header data={{ text: 'Header from PDP' }} />
       <div className="my-10 ">
-        <Routes>
-          <Route path="/product/:id" element={<PDPContent />} />
-        </Routes>
+        <Switch>
+          <Route path="/product/:id" component={<PDPContent />} />
+        </Switch>
       </div>
       <Footer />
     </div>
